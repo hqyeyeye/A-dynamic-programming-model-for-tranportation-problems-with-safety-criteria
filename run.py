5aa143ee88197b64d12d67446d9f03c95da29cf9
hqyeyeye <30504520+hqyeyeye@users.noreply.github.com>
2024-02-29 19:45:06 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,111 +1,120 @@
 '''run file'''
 from valueiteration_write import ValueIterationModel
-#from PetModel import PetModel
 import matplotlib.pyplot as plt
-#a = PetModel()
 ''' change different p and q '''
 
-p = 1
-q = 1
-g = 1.5
-gNone = 2
+p = 1.0
+q = 1.0
+g = 0
 
-# =============================================================================
-# p = float(input("Please insert p: "))
-# q = float(input("Please insert q: "))
-# =============================================================================
-#print("\nThe result of original optimization: ")
-#
-#''' If you want to use a different dataset, replacing 'data.xlsx' with your filename'''
-#
-#a.readData('g_5.22.xlsx',0)
-#print(a.parameter)
-#print(a.stage)
-#print(a.distance)
-#a.printStage()
-#a.ObjectiveOptimization(p,q)
-#import matplotlib.pyplot as plt
-#a.readData('data.xlsx',1)
-''' change first argument to try a different speed '''
-#a.NoStop(75,p,q)
-#a.NoRisk(75,p,q)
-#print("\nThe result of value iteration: ")
-b = ValueIterationModel(p,q,g,gNone)
-b.readData('data_6.21.xlsx',0)
-print('gNone is', gNone, ',', 'g is', g )
-g1,c1 = b.Optimizer(num_of_stage = 16)
-###g4,c4 = b.Go([0, 55, 0, 75, 75, 75, 75, 75, 75, 55, 75, 55, 55, 0, 0, 75] )
-#
-#print('---------------------------------')
-#g2,c2 = b.Optimizer(1)
-#if g2 == None:
-#    g2 = [0] * len(g1)
-#    c2 = [0] * len(g1)
-#print('---------------------------------')
-#g3,c3 = b.NoStop()
-#
-##
-#fig, axs = plt.subplots(2,1)   
-#
-##axs[0].plot(g1, '-rD', g2, '-bs', g3, '-g^')
-#axs[0].plot(g1, '-r', label = 'optimal')
-##axs[0].plot(g4, '-rD', label = 'certain')
-#axs[0].plot(g2, '--b', label = 'no risk')
-#axs[0].plot(g3, '-.g', label = 'no stop')
-#axs[0].set_title('g plot')
-#axs[0].set_xlabel('stage')
-#axs[0].set_ylabel('g')
-##axs[1].plot(c1, '-rD', c2, '-bs', c3, '-g^')
-#axs[1].plot(c1, '-rD', label = 'optimal')
-##axs[1].plot(c4, '-rD', label = 'certain')
-#axs[1].plot(c2, '-bs', label = 'no risk')
-#axs[1].plot(c3, '-g^', label = 'no stop')
-#axs[1].set_xlabel('stage')
-#axs[1].set_title('cost plot')
-#axs[1].set_ylabel('cost')
-#plt.subplots_adjust(hspace=0.8)
-#fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.00), shadow=True, ncol=2)
-#fig.show()
-#q_list=[0.01,0.02,0.03,0.04,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
-#        23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
-#b.pqratio_plot(q_list,16)
-#q_list=[0.01,0.02,0.03,0.04,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,
-#        ]
-#b.pqratio_plot(q_list,16)    
+print("\nThe result of value iteration: ")
+b = ValueIterationModel(p,q,g)
+#first case
+print ("Case 1:")
+b.readData('data_6.21.xlsx',1)
+#g11, c11 is the first case and the optimal policy
+g11,c11 = b.Optimizer(num_of_stage = 16)
 
-#fig,axs = plt.subplots() 
-#axs.plot(c1, '-ko', label = 'optimal')
-##axs.plot(g4, '-bo', label = 'starting when g is 0')
-##
-##axs.set_title('g plot')
-#axs.set_xlabel('stage')
-#axs.set_ylabel('cost')
-#legend = fig.legend(loc='upper right', shadow=True, fontsize = 'x-large', ncol=2)
-#legend.get_frame().set_facecolor('white')
-#fig.show()
-#
-#
-#fig,axs = plt.subplots() 
-#axs.plot(c1, '-ko', label = 'optimal')
-#axs.plot(c4, '-bo', label = 'starting when g is 0')
-#axs.set_title('cost plot')
-#axs.set_xlabel('stage')
-#axs.set_ylabel('cost')
-#legend = fig.legend(loc='upper right', shadow=True, fontsize = 'x-large', ncol=2)
-#legend.get_frame().set_facecolor('white')
-#fig.show()
+print('---------------------------------')
+g12,c12 = b.Optimizer(1)
+if g12 == None:
+    g12 = [0] * len(g11)
+    c12 = [0] * len(g11)
+print('---------------------------------')
+g13,c13 = b.NoStop()
+
+#second case
+print ("Case 2:")
+b.readData('data_6.21.xlsx',2)
+#g21, c21 is the second case and the optimal policy
+g21,c21 = b.Optimizer(num_of_stage = 16)
+
+print('---------------------------------')
+g22,c22 = b.Optimizer(1)
+if g22 == None:
+    g22 = [0] * len(g21)
+    c22 = [0] * len(g21)
+print('---------------------------------')
+g23,c23 = b.NoStop()
+
+#third case
+print ("Case 3:")
+b.readData('data_6.21.xlsx',3)
+#g31, c31 is the third case and the optimal policy
+g31,c31 = b.Optimizer(num_of_stage = 16)
+
+print('---------------------------------')
+g32,c32 = b.Optimizer(1)
+if g32 == None:
+    g32 = [0] * len(g31)
+    c32 = [0] * len(g31)
+print('---------------------------------')
+g33,c33 = b.NoStop()
+
+#fourth case
+print ("Case 4:")
+b.readData('data_6.21.xlsx',4)
+#g41, c41 is the first case and the optimal policy
+g41,c41 = b.Optimizer(num_of_stage = 16)
+
+print('---------------------------------')
+g42,c42 = b.Optimizer(1)
+if g42 == None:
+    g42 = [0] * len(g41)
+    c42 = [0] * len(g41)
+print('---------------------------------')
+g43,c43 = b.NoStop()
 
 
+fig, axs = plt.subplots()   
+
+axs.plot(g11, '-ko', label = 'case 1 optimal')
+axs.plot(g12, '--k^', label = 'case 1 no risk')
+axs.plot(g13, '-.kd', label = 'case 1 no stop')
+
+axs.plot(g21, '-bo', label = 'case 2 optimal')
+axs.plot(g22, '--b^', label = 'case 2 no risk')
+axs.plot(g23, '-.bd', label = 'case 2 no stop')
+
+axs.plot(g31, '-go', label = 'case 3 optimal')
+axs.plot(g32, '--g^', label = 'case 3 no risk')
+axs.plot(g33, '-.gd', label = 'case 3 no stop')
+
+axs.plot(g41, '-ro', label = 'case 4 optimal')
+axs.plot(g42, '--r^', label = 'case 4 no risk')
+axs.plot(g43, '-.rd', label = 'case 4 no stop')
+
+axs.set_title('g plot')
+axs.set_xlabel('stage')
+axs.set_ylabel('g')
+plt.subplots_adjust(hspace=0.8)
+legend = fig.legend(loc='upper right', shadow=True, fontsize = 'x-large')
+legend.get_frame().set_facecolor('white')
+fig.show()
+
+fig, axs = plt.subplots()
+axs.plot(c11, '-ko', label = ' case 1 optimal')
+axs.plot(c12, '--k^', label = 'case 1 no risk')
+axs.plot(c13, '-.kd', label = 'case 1 no stop')
+
+axs.plot(c21, '-bo', label = ' case 2 optimal')
+axs.plot(c22, '--b^', label = 'case 2 no risk')
+axs.plot(c23, '-.bd', label = 'case 2 no stop')
+
+axs.plot(c31, '-go', label = ' case 3 optimal')
+axs.plot(c32, '--g^', label = 'case 3 no risk')
+axs.plot(c33, '-.gd', label = 'case 3 no stop')
+
+axs.plot(c41, '-ro', label = ' case 4 optimal')
+axs.plot(c42, '--r^', label = 'case 4 no risk')
+axs.plot(c43, '-.rd', label = 'case 4 no stop')
 
 
-# =============================================================================
-# decision = None
-# while (decision != 'y' and decision != 'n'): 
-#     decision = raw_input("Do you want to print all states(y/n): ")
-#     if decision == 'y':
-#         print("\nAll states will be printed below\n")
-#         a.printStage()
-#     elif decision == 'n':
-#         break
-# =============================================================================
-        
+axs.set_xlabel('stage')
+axs.set_title('cost plot')
+axs.set_ylabel('cost')
+plt.subplots_adjust(hspace=0.8)
+legend = fig.legend(loc='upper right', shadow=True, fontsize = 'x-large')
+legend.get_frame().set_facecolor('white')
+fig.show()
+  
